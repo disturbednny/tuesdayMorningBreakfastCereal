690c96a00a6aec4847f4fb7ee58b294097ffb6b6
Johnathan Arsenault <johnathan.arsenault@gmail.com>
2023-09-19 23:31:57 -0500

    rebase cleanup

--- src/main/groovy/org/disturbednny/cereal/cerealSpringBootRest/pojo/PressureSensor.groovy
+++
@@ -1,8 +0,0 @@
-package org.disturbednny.cereal.cerealSpringBootRest.pojo
-
-class PressureSensor extends Sensor {
-    private double pressure
-    private String UoM
-
-    // TODO: normalize on UoM to be sent to the database. for now we are only going to use bar
-}
--- src/main/groovy/org/disturbednny/cereal/cerealSpringBootRest/pojo/Sensor.groovy
+++
@@ -1,8 +0,0 @@
-package org.disturbednny.cereal.cerealSpringBootRest.pojo
-
-class Sensor {
-
-    private String name
-    private String type
-
-}
